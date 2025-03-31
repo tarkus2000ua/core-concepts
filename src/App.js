64b7c5a3035c69a56edcdded91c6bed550d9185f
Andrii Moskalenko <Andrii.Moskalenko@ihsmarkit.com>
2025-03-31 17:00:39 +0300

    movie tile component added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import './App.css';
 import { Counter } from './components/counter/counter';
+import MovieTile from './components/movie-tile/movie-tile';
 import OptionSelect from './components/option-select/option-select';
 import SearchForm from './components/search-form/search-form';
-
-const GENRES = [
-  {
-    id: 1,
-    value: 'ALL',
-  },
-  {
-    id: 2,
-    value: 'DOCUMENTARY',
-  },
-  {
-    id: 3,
-    value: 'COMEDY',
-  },
-  {
-    id: 4,
-    value: 'HORROR',
-  },
-  {
-    id: 5,
-    value: 'CRIME',
-  },
-];
+import { GENRES, MOVIE } from './demo.constants';
 
 function App() {
   const onSearch = (value) => console.log(`Text to search: ${value}`);
   const onSelect = (value) => console.log(`Genre selected: ${value}`);
+  const onTileClick = (value) => console.log(`Movie selected: ${value.title}`);
   return (
     <div className="App">
       <h2>Counter</h2>
@@ -40,6 +20,10 @@ function App() {
       <h2>Genre Select</h2>
       <div className='center'>
         <OptionSelect options={GENRES} onSelect={onSelect} />
+      </div>
+      <h2>Movie Tile</h2>
+      <div className='center'>
+        <MovieTile movie={MOVIE} onTileClick={onTileClick} />
       </div>
     </div>
   );
---
+++ src/demo.constants.js
@@ -0,0 +1,35 @@
+export const GENRES = [
+  {
+    id: 1,
+    value: 'ALL',
+  },
+  {
+    id: 2,
+    value: 'DOCUMENTARY',
+  },
+  {
+    id: 3,
+    value: 'COMEDY',
+  },
+  {
+    id: 4,
+    value: 'HORROR',
+  },
+  {
+    id: 5,
+    value: 'CRIME',
+  },
+];
+
+export const MOVIE = {
+  id: 30,
+  title: 'Pulp Fiction',
+  year: '1994',
+  runtime: '154',
+  genres: ['Crime', 'Drama'],
+  director: 'Quentin Tarantino',
+  actors: 'Tim Roth, Amanda Plummer, Laura Lovelace, John Travolta',
+  plot: "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+  posterUrl:
+    'https://images-na.ssl-images-amazon.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_SX300.jpg',
+};
